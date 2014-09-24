38311470f3674246ab879b04dbe00deeefd582be
Jacob Truman <jacob.truman@gmail.com>
2014-09-24 10:13:06 -0600

    Add date flag to enable prepending message with date/time

--- logger.js
+++ logger.js
@@ -1,53 +1,66 @@
 require('colors');
 
-function logger(logFile){
+function logger(logFile, date){
 	this.logFile = logFile;
+	if(date !== undefined) {
+		this.date = date;
+	} else {
+		this.date = false;
+	}
 }
 
 var p = logger.prototype;
 var fs = require('fs');
 
 p.error = function(msg){
-	msg = "ERROR: "+msg.toString();
+	msg = this.buildMsg("ERROR: "+msg.toString());
 	console.log((msg).red);
 	this.append(msg);
 }
 
 p.info = function(msg){
-	msg = "INFO: "+msg.toString();
+	msg = this.buildMsg("INFO: "+msg.toString());
 	console.log((msg).cyan);
 	this.append(msg);
 }
 
 p.warning = function(msg){
-	msg = "WARNING: "+msg.toString();
+	msg = this.buildMsg("WARNING: "+msg.toString());
 	console.log((msg).yellow);
 	this.append(msg);
 }
 
 p.success = function(msg){
-	msg = "SUCCESS: "+msg.toString();
+	msg = this.buildMsg("SUCCESS: "+msg.toString());
 	console.log((msg).green);
 	this.append(msg);
 }
 
 p.chat = function(msg){
-	msg = "CHAT: "+msg.toString();
+	msg = this.buildMsg("CHAT: "+msg.toString());
 	console.log((msg).magenta);
 	this.append(msg);
 }
 
 p.add = function(msg){
-	msg = msg.toString();
+	msg = this.buildMsg(msg.toString());
 	console.log((msg).grey);
 	this.append(msg);
 }
 
-p.append = function(msg){
+p.append = function(msg)
+{
 	fs.appendFile(this.logFile, msg+"\n", function (err) {
 		if (err) throw err;
-		//console.log('The "'+msg+'" was appended to file!');
 	});
+}
+
+p.buildMsg = function(msg) {
+	if (this.date) {
+		var date = new Date();
+		msg = date.toJSON() + " :: " + msg.toString();
+	}
+	return msg;
 }
 
 module.exports = logger;
